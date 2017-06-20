f84bdeb195be44842f0960a87cdb974b81361ddf
Kim Arundale <arundale@iastate.edu>
2017-06-20 13:55:04 -0500

    Changed naming conventions, added a few comments, and fixed one bug

--- angularTictac/app.js
+++ angularTictac/app.js
@@ -1,40 +1,41 @@
-
-//this creates module of ng
+// This creates module of ng
 var app = angular.module('handsome', ['ng']);
-//this allows the board to be created
+
+// This allows the board to be created
 app.controller('BoardCtrl', function($scope, Board){
 	$scope.board = new Board;
 });
+
 (function() {
-	var Board = (function(){
-		var SIZE=3;
-		var EMPTY=' ';
-		var Orange='O';
-     	var Xmen='X'; 
-		var marker=[Orange, Xmen];
-        var	Cell;
-        //this starts the board
+	var Board = (function() {
+		var SIZE = 3;						// 3 x 3 board
+		var EMPTY = ' ';					// Empty fill space
+		var Player1 = 'O';					// Player1 (O)
+     	var Player2 = 'X'; 					// Player2 (X)
+		var marker = [Player1, Player2];	// To track current player
+        var	Cell;							// A cell is a single box on the board
+		
+        // This starts the board
 		function Board() {
 			this.reset();
 		}
 
 		Cell = (function() {
-			function Cell(jordan){
-				this.jordan = jordan;
+			function Cell(player) {
+				this.player = player;
 				this.winning = false;
 			}
-			Cell.prototype.mark = function(jordan) {
-				this.jordan = jordan;
+			Cell.prototype.mark = function(player) {
+				this.player = player;
 			}
 			Cell.prototype.hasIt = function() {
-				return this.jordan != EMPTY;
+				return this.player != EMPTY;
 			}
 			return Cell;
 		})();
 
-		
-
-		Board.prototype.reset = function(){
+		// Resetting the board for new game
+		Board.prototype.reset = function() {
 			this.currentPlayer = 0;
 			this.grid = [];
 			this.cute = false;
@@ -46,111 +47,114 @@ app.controller('BoardCtrl', function($scope, Board){
 				for(var j=0; j < SIZE; j++) {
 					row.push(new Cell(EMPTY));
 				}
-
 				this.grid.push(row);
 			}
-
 			return this.grid;
 		};
 
 		Board.prototype._checkRow = function(rowIndex) {
-			var numOrange, numXmen = 0;
-			
+			var numPlayer1 = 0;
+			var numPlayer2 = 0;
 
 			for(var columnIndex = 0; columnIndex < SIZE; columnIndex++) {
 				var cell = this.grid[rowIndex];
 				cell = cell[columnIndex];
-				var cellMarker = cell.jordan;
+				var cellMarker = cell.player;
 				if(cellMarker == EMPTY) {
 					return false;
 				}
-				if(cellMarker == Orange) {
-					numOrange++;
-				} else if(cellMarker == Xmen) {
-					numXmen++;
+				
+				if(cellMarker == Player1) {
+					numPlayer1++;
+				} else if(cellMarker == Player2) {
+					numPlayer2++;
 				}
 			}
 
-			if(numOrange == SIZE) {
-				return Orange;
-			} else if(numXmen == SIZE) {
-				return Xmen;
+			if(numPlayer1 == SIZE) {
+				return Player1;
+			} else if(numPlayer2 == SIZE) {
+				return Player2;
 			}
 		};
-//a bunch of check winner conditions
+		
+		// A bunch of check winner conditions
 		Board.prototype._checkColumn = function(columnIndex) {
-			var numOrange = 0;
-			var numXmen = 0;
+			var numPlayer1 = 0;
+			var numPlayer2 = 0;
 
 			for(var rowIndex = 0; rowIndex < SIZE; rowIndex++) {
 				var cell = this.grid[rowIndex][columnIndex];
-				var cellMarker = cell.jordan;
+				var cellMarker = cell.player;
 				if(cellMarker == EMPTY) {
 					return false;
 				}
-				if(cellMarker == Orange) {
-					numOrange++;
-				} else if(cellMarker == Xmen) {
-					numXmen++;
+				
+				if(cellMarker == Player1) {
+					numPlayer1++;
+				} else if(cellMarker == Player2) {
+					numPlayer2++;
 				}
 			}
 
-			if(numOrange == SIZE) {
-				return Orange;
-			} else if(numXmen == SIZE) {
-				return Xmen;
+			if(numPlayer1 == SIZE) {
+				return Player1;
+			} else if(numPlayer2 == SIZE) {
+				return Player2;
 			}
 		}
 
 		Board.prototype._checkDiagonal1 = function() {
-			var numOrange = 0;
-			var numXmen = 0;
+			var numPlayer1 = 0;
+			var numPlayer2 = 0;
 
 			for(var i = 0; i<SIZE; i++) {
-				var cellMarker = this.grid[i][i].jordan;
+				var cellMarker = this.grid[i][i].player;
 				if(cellMarker == EMPTY) {
 					return false;
 				}
-				if(cellMarker == Orange) {
-					numOrange++;
-				} else if(cellMarker == Xmen) {
-					numXmen++;
+				
+				if(cellMarker == Player1) {
+					numPlayer1++;
+				} else if(cellMarker == Player2) {
+					numPlayer2++;
 				}
 			}
 
-			if(numOrange == SIZE) {
-				return Orange;
-			} else if(numXmen == SIZE) {
-				return Xmen;
+			if(numPlayer1 == SIZE) {
+				return Player1;
+			} else if(numPlayer2 == SIZE) {
+				return Player2;
 			}
 		}
 
 		Board.prototype._checkDiagonal2 = function() {
-			var numOrange = 0;
-			var numXmen = 0;
+			var numPlayer1 = 0;
+			var numPlayer2 = 0;
 
 			for(var i = 0; i<SIZE; i++) {
-				var cellMarker = this.grid[i][SIZE-i-1].jordan;
+				var cellMarker = this.grid[i][SIZE-i-1].player;
 				if(cellMarker == EMPTY) {
 					return false;
 				}
-				if(cellMarker == Orange) {
-					numOrange++;
-				} else if(cellMarker == Xmen) {
-					numXmen++;
+				
+				if(cellMarker == Player1) {
+					numPlayer1++;
+				} else if(cellMarker == Player2) {
+					numPlayer2++;
 				}
 			}
 
-			if(numOrange == SIZE) {
-				return Orange;
-			} else if(numXmen == SIZE) {
-				return Xmen;
+			if(numPlayer1 == SIZE) {
+				return Player1;
+			} else if(numPlayer2 == SIZE) {
+				return Player2;
 			}
 		}
 
-		Board.prototype._setWinner = function(jordan) {
+		Board.prototype._setWinner = function(player) {
 			this.cute = true;
-			this.winning_jordan = jordan;
+			this.winning_player = player;
 			this.continueing = false;
 		};
 
@@ -177,9 +181,7 @@ app.controller('BoardCtrl', function($scope, Board){
 				this.grid[i][SIZE-i-1].winning = true;
 			}
 		}
-
-
-
+		
 		Board.prototype.checkWinner = function() {
 			var rowIndex = 0;
 			var columnIndex = 0;
@@ -214,37 +216,37 @@ app.controller('BoardCtrl', function($scope, Board){
 				this._markWinnerDiagonal2();
 			}
 		};
-		//this marks the player on board
+		
+		// Mark the player on board - game switches players once a player
+		// places their mark (X or O) in a cell
         Board.prototype.markDa = function() {
 			return marker[this.currentPlayer];
 		}
-		//this check if the cells is already marked or if game is still on
+		
+		// Check if the cells are already marked or if game is still on
 		Board.prototype.playCell = function(cell) {
 			if(!(this.continueing)) {
 				return;
 			}
+			
 			if(cell.hasIt()) {
 				return;
 			}
+			
 			cell.mark(this.markDa());
 			this.switcher();
 			this.checkWinner();
-			
 		};
-
-		
 
 		Board.prototype.switcher = function() {
 			this.currentPlayer = Math.abs(this.currentPlayer-1);
 		}
-
 		return Board;
 	})();
 
-	angular.module('handsome').factory('Board', function(){
+	angular.module('handsome').factory('Board', function() {
 		return Board;
 	});
-
 }).call(this);
 
 

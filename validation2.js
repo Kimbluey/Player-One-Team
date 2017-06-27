41a1129488871e5328f4e3718544811b43a94e17
Kim Arundale <arundale@iastate.edu>
2017-06-27 16:32:03 -0500

    Adding more to validation; also made some formatting fixes

--- validation2.js
+++ validation2.js
@@ -1,81 +1,100 @@
+// Form validation for user login and sign up
 function validate2() {
-    valCheck = true;
-    var image1 = getImage(emailCheck(document.forms["contact information"]["email"].value), "email");
-    document.getElementById("Email").appendChild(image1);
-	var imagel2 = getImage(alphaNumCheck(document.forms["contact information"]["phone"].value), "phone");
-    document.getElementById("Phone").appendChild(imagel2);
+	valCheck = true;
+	var image1 = getImage(emailCheck(document.forms["entries"]["email"].value), "email");
+	document.getElementById("Email").appendChild(image1);
 	
-if(emailCheck(document.forms["contact information"]["email"].value) && alphaNumCheck(document.forms["contact information"]["phone"].value))
-  window.location.href = "index.html";
+	var image2 = getImage(alphaNumCheck(document.forms["entries"]["password"].value), "password");
+	document.getElementById("Password").appendChild(image2);
+	
+	var image3 = getImage(nameCheck(document.forms["entries"]["fname"].value), "fname");
+	document.getElementByID("FName").appendChild(image3);
+	
+	var image4 = getImage(nameCheck(document.forms["entries"]["lname"].value), "lname");
+	document.getElementByID("LName").appendChild(image4);
+	
+	if (emailCheck(document.forms["entries"]["email"].value) && alphaNumCheck(document.forms["entries"]["password"].value)
+	    && nameCheck(document.forms["entries"]["fname"].value) && nameCheck(document.forms["entries"]["lname"].value)) {
+		
+		window.location.href = "index.html";
+	}
 }
 
-
+// Images to display for valid (correct.png) and invalid (wrong.png) entries
 function getImage(bool, ID) {
-    var image = document.getElementById("image" + ID);
-    if (image == null) {
-        image = new Image(15, 15);
-        image.id = "image" + ID;
-    }
-    image.src = bool ? './correct.png' : './wrong.png';
-    return image;
-}
-function addressCheck(address) {
-    atSplit = address.split(',');
-	if(atSplit.length==2)
-		return true;
-	
-    
-    return false;
+	var image = document.getElementById("image" + ID);
+	if (image == null) {
+		image = new Image(15, 15);
+		image.id = "image" + ID;
+	}
+	image.src = bool ? './correct.png' : './wrong.png';
+	return image;
 }
 
+// Check for valid email
 function emailCheck(email) {
-    atSplit = email.split('@');
-    if (atSplit.length == 2 && alphaNumCheck(atSplit[0])) {
-        periodSplit = atSplit[1].split('.')
-        if (periodSplit.length == 2 && alphaNumCheck(periodSplit[0] + periodSplit[1])) {
-            return true;
-        }
-    }
-    valCheck = false;
-    return false;
+	atSplit = email.split('@');
+	if (atSplit.length == 2 && alphaNumCheck(atSplit[0])) {
+		periodSplit = atSplit[1].split('.')
+		if (periodSplit.length == 2 && alphaNumCheck(periodSplit[0] + periodSplit[1])) {
+			return true;
+		}
+	}
+	valCheck = false;
+	return false;
 }
 
+// Check for valid name values
+function nameCheck(entry) {
+	let regex = /^[A-Z]+$/i;
+	if (entry != null && entry.match(regex)) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
+// Check for valid alphanumeric values
 function alphaNumCheck(entry) {
-    let regex = /^[a-z0-9]+$/i;
-    if (entry != null && entry.match(regex)) {
-        return true;
-    } else {
-        return false;
-    }
+	let regex = /^[a-z0-9]+$/i;
+	if (entry != null && entry.match(regex)) {
+		return true;
+	} else {
+		return false;
+	}
 }
+
+// Check for valid numeric values
 function numCheck(entry) {
-    let regex = /^[0-9]+$/i;
-    if (entry != null && entry.match(regex)) {
-        return true;
-    } else {
-        return false;
-    }
+	let regex = /^[0-9]+$/i;
+	if (entry != null && entry.match(regex)) {
+		return true;
+	} else {
+		return false;
+	}
 }
-function phoneCheck(phone){
-	if(phone.length==0){
-	return true;}
-	if(phone.length== 10&& numCheck(phone)){
+
+// Check for valid password
+function passwordCheck(password){
+	// Can't have password of length zero
+	if(password.length == 0){
 		return true;
 	}
-	atSplit=phone.split('-');
+	// Password must be of length 10 and numeric
+	if(password.length == 10 && numCheck(password)){
+		return true;
+	}
+	atSplit=password.split('-');
 	if(atSplit.length== 3 && numCheck(atSplit[0]) && numCheck(atSplit[1]) && numCheck(atSplit[2]) && atSplit[0].length==3 && atSplit[1].length== 3 && atSplit[2].length==4)
 	{
-	
-	return true;
+		return true;
 	}
 	valCheck=false;
 	return false;
 }
 
+// Delete cookies
 function deleteCookie( name ) {
   document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
-
-
 
